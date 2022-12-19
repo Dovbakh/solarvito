6941510036df7d159a513046a7caa5b1b2a5b365
Svyatoslav <dovbakhs@yandex.ru>
2022-12-19 21:27:18 +0300

    Резервный коммит

--- src/Solarvito/Infrastructure/Solarvito.Migrations/Migrations/20221219094412_Delete_count_columns_in_User_table.Designer.cs
+++ src/Solarvito/Infrastructure/Solarvito.Migrations/Migrations/20221219094412_Delete_count_columns_in_User_table.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using Solarvito.Migrations;
@@ -11,9 +12,11 @@ using Solarvito.Migrations;
 namespace Solarvito.Migrations.Migrations
 {
     [DbContext(typeof(MigrationsDbContext))]
-    partial class MigrationsDbContextModelSnapshot : ModelSnapshot
+    [Migration("20221219094412_Delete_count_columns_in_User_table")]
+    partial class DeletecountcolumnsinUsertable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/Solarvito/Infrastructure/Solarvito.Migrations/Migrations/20221219094412_Delete_count_columns_in_User_table.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Solarvito.Migrations.Migrations
+{
+    /// <inheritdoc />
+    public partial class DeletecountcolumnsinUsertable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "NumberOfRates",
+                table: "Users");
+
+            migrationBuilder.DropColumn(
+                name: "Rating",
+                table: "Users");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "NumberOfRates",
+                table: "Users",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.AddColumn<float>(
+                name: "Rating",
+                table: "Users",
+                type: "real",
+                nullable: false,
+                defaultValue: 0f);
+        }
+    }
+}
